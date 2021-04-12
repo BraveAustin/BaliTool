bb1db5ad426d98275fe387a07f528566dfbe389f
BraveAustin <610952927@QQ.com>
2021-04-12 22:10:09 +0800

    LocalDateTimeUtil Pull

---
+++ src/main/java/cn/bali/tool/time/LocalDateTimeUtil.java
@@ -0,0 +1,29 @@
+package cn.bali.tool.time;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @Author BraveAustin
+ * @Description //TODO
+ * @Date 2021/4/12 0012
+ **/
+public class LocalDateTimeUtil {
+
+    /**
+     * 返回相应格式的String
+     * @param localDateTime
+     * @param pattern
+     * @return
+     */
+    public static String getFormatString(LocalDateTime localDateTime,String pattern){
+        if (localDateTime == null || pattern == null) {
+            return null;
+        }
+        if (null != localDateTime) {
+            return localDateTime.format(DateTimeFormatter.ofPattern(pattern));
+        }
+        return null;
+    }
+
+}
